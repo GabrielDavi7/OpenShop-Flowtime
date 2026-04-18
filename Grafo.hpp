617f6521ead7db74b92dc6b174d8193de400cbf7
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2026-04-18 17:30:18 -0300

    feat: adiciona classe Grafo/metodos base

--- Grafo.hpp
+++ Grafo.hpp
@@ -7,7 +7,7 @@ struct Vertice{
     int id; 
     int peso; 
     int grau_entrada; // Numero de aretas que apontam para o vertice
-    int temp_termino;   //tempo acumulado para o termino do vertice
+    int tempo_termino;   //tempo acumulado para o termino do vertice
     int predecessor_maximo;  // id do vertice predecessor que tem o maior tempo acumulado para o termino
     std::vector<int> adjacentes; // uma lista que mostra para quem o vertice aponta
 
@@ -15,9 +15,20 @@ struct Vertice{
         id = 0;
         peso = 0;
         grau_entrada = 0;
-        temp_termino = 0;
+        tempo_termino = 0;
         predecessor_maximo = -1; // -1 indica que não tem predecessor
     }
+};
+
+class Grafo{
+    public:
+        int num_vertices;
+        std::vector<Vertice> vertices;
+
+        Grafo(int n);
+        void adicionarAresta(int origem, int destino);
+        void caminhadaTopologica();
+        void imprimirCaminhoMaximo(int destino);
 };
 
 #endif
