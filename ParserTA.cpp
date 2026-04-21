06c891d8775c95cb9ee5edd8158a0bf085e8e11b
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2026-04-21 17:31:39 -0300

    fix: muda leitura instancias

--- ParserTA.cpp
+++ ParserTA.cpp
@@ -4,27 +4,55 @@
 #include <string>    
 
 Instancia ParserTA::lerArquivo(const std::string& caminhoArquivo) {
+    
     Instancia instancia;
+    instancia.num_trabalhos = 0;
+    instancia.num_maquinas = 0;
+
     std::ifstream arquivo(caminhoArquivo);
-    std::string lixo;
+    std::string palavra;
 
     if(!arquivo.is_open()) {
         std::cerr << "Erro ao abrir o arquivo: " << caminhoArquivo << std::endl;
-        return instancia; // Retorna uma instância vazia em caso de erro
+        return instancia;
     }
 
-    arquivo>>lixo>>instancia.num_trabalhos>>instancia.num_maquinas; // Lê o número de trabalhos e máquinas, ignorando a palavra "trabalhos" e "maquinas"
-    arquivo>>lixo; // pula a palavra "custos"
+    // Fica lendo o arquivo até achar "TotalJobs:"
+    while (arquivo >> palavra) {
+        if (palavra == "TotalJobs:") {
+            arquivo >> instancia.num_trabalhos; // O próximo item é o número
+            break; 
+        }
+    }
 
-    instancia.custos.resize(instancia.num_trabalhos, std::vector<int>(instancia.num_maquinas));
+    // Continua lendo até achar "TotalMachines:"
+    while (arquivo >> palavra) {
+        if (palavra == "TotalMachines:") {
+            arquivo >> instancia.num_maquinas; // O próximo item é o número
+            break; 
+        }
+    }
 
-    for(int i = 0; i < instancia.num_trabalhos; ++i) {
-        for(int j = 0; j < instancia.num_maquinas; ++j) {
-           if(!(arquivo >> instancia.custos[i][j])) {
-                break; // Sai do loop se não conseguir ler um custo, o que pode indicar um formato de arquivo incorreto
+    // Continua lendo até achar "Costs:"
+    while (arquivo >> palavra) {
+        if (palavra == "Costs:") {
+            break; 
+        }
+    }
+
+    // Le a matriz apenas se os números de trabalhos e máquinas forem válidos
+    if (instancia.num_trabalhos > 0 && instancia.num_maquinas > 0) {
+        instancia.custos.resize(instancia.num_trabalhos, std::vector<int>(instancia.num_maquinas));
+
+        for(int i = 0; i < instancia.num_trabalhos; ++i) {
+            for(int j = 0; j < instancia.num_maquinas; ++j) {
+               if(!(arquivo >> instancia.custos[i][j])) {
+                   break;
+                }
             }
         }
     }
+
     arquivo.close();
     return instancia;
 }
