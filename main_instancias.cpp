1314f4a03159bd8cf52346a0e840cbcc1b799aa2
Gabriel Davi <gabrielbaiamota765@gmail.com>
2026-04-24 17:11:00 -0300

    feat: flowtime baseline

--- main_instancias.cpp
+++ main_instancias.cpp
@@ -37,5 +37,36 @@ int main() {
     cout << "O tempo da ultima operacao (Trabalho " << n-1 << ", Maquina " << m-1 << ") eh: " 
          << grafo.getPeso(total_custo) << endl;
     
+    for(int maquina=0; maquina < m; maquina++){
+        for(int trabalho=0; trabalho < n-1; trabalho++){
+            int origem = (trabalho * m) + maquina + 1; // ID do vértice da operação atual
+            int destino = ((trabalho + 1) * m) + maquina + 1; // ID do vértice da próxima operação na mesma máquina
+            grafo.adicionarAresta(origem, destino); // Adiciona uma aresta do vértice atual para o próximo vértice na mesma máquina
+        }
+    }
+
+    for(int trabalho=0; trabalho < n; trabalho++){
+        for(int maquina=0; maquina < m-1; maquina++){
+            int origem = (trabalho * m) + maquina + 1; // ID do vértice da operação atual
+            int destino = (trabalho * m) + maquina + 2; // ID do vértice da próxima operação no mesmo trabalho
+            grafo.adicionarAresta(origem, destino); // Adiciona uma aresta do vértice atual para o próximo vértice no mesmo trabalho
+        }
+    }
+
+    cout << "Realizando caminhada topologica para calcular o caminho máximo..." << endl;
+    grafo.caminhadaTopologica();
+
+    int flowtime = 0;
+    for(int trabalho=0; trabalho < n; trabalho++){
+        int idUltimaOperacao = (trabalho * m) + m; // ID do vértice da última operação de cada trabalho
+        int tempoTermino = grafo.vertices[idUltimaOperacao].tempo_termino;
+        cout << "Tempo de término do Trabalho " << trabalho << ": " << tempoTermino << endl;
+        flowtime += tempoTermino;
+    }
+
+    cout << "\n=========================================" << endl;
+    cout << "FLOWTIME TOTAL (Baseline): " << flowtime << endl;
+    cout << "=========================================" << endl;
+
     return 0;
 }   
